5ecad8963ea1864592d97e75b5964233c8328447
Evelyn Cassinotte <cassinotte@gmail.com>
2026-05-05 19:27:33 -0300

    feat: adicionado gráfico de colunas a pagina meu desempenho

--- NotasMax/ViewModels/ViewModelExample.cs
+++ NotasMax/ViewModels/ViewModelExample.cs
@@ -16,27 +16,31 @@ public class NotaMateria
 
 public class ViewModelExemplo
 {
-    public ObservableCollection<NotaSimulado> NotaSimulados { get; set; }
-    public ObservableCollection<NotaMateria> NotaMaterias { get; set; }
+    public List<NotaSimulado> NotaSimulados { get; set; }
+    public List<NotaMateria> NotaMaterias { get; set; }
     public ViewModelExemplo()
     {
         // Gerando alguns dados de exemplo (Mock / Simulação) para o gráfico de desempenho
-        NotaSimulados = new ObservableCollection<NotaSimulado>
+        NotaSimulados = new List<NotaSimulado>
         {
-            new NotaSimulado { Numero = "1", Nota = 6.5 },
-            new NotaSimulado { Numero = "2", Nota = 7.0 },
-            new NotaSimulado { Numero = "3", Nota = 6.0 },
-            new NotaSimulado { Numero = "4", Nota = 8.5 },
-            new NotaSimulado { Numero = "5", Nota = 9.0 }
+            new NotaSimulado { Numero = "1", Nota = 6.54 },
+            new NotaSimulado { Numero = "2", Nota = 7.45 },
+            new NotaSimulado { Numero = "3", Nota = 6.30 },
+            new NotaSimulado { Numero = "4", Nota = 8.56 },
+            new NotaSimulado { Numero = "5", Nota = 9.14 }
         };
 
-        NotaMaterias = new ObservableCollection<NotaMateria>
+        NotaMaterias = new List<NotaMateria>
         {
             new NotaMateria { Materia = "Matemática", Nota = 7.5 },
             new NotaMateria { Materia = "Português", Nota = 8.0 },
-            new NotaMateria { Materia = "Ciências", Nota = 6.5 },
+            new NotaMateria { Materia = "Química", Nota = 6.5 },
             new NotaMateria { Materia = "História", Nota = 7.0 },
-            new NotaMateria { Materia = "Geografia", Nota = 8.5 }
+            new NotaMateria { Materia = "Geografia", Nota = 8.5 },
+            new NotaMateria { Materia = "Inglês", Nota = 3.5 },
+            new NotaMateria { Materia = "Física", Nota = 8.0 },
+            new NotaMateria { Materia = "Biologia", Nota = 6.5 }
+
         };
 
 
--- NotasMax/Views/Aluno/MeuDesempenhoView.xaml.cs
+++ NotasMax/Views/Aluno/MeuDesempenhoView.xaml.cs
@@ -5,23 +5,65 @@ namespace NotasMax.Views.Aluno;
 
 public partial class MeuDesempenhoView : ContentPage
 {
-	public MeuDesempenhoView()
-	{
-		InitializeComponent();
-	}
+    public MeuDesempenhoView()
+    {
+        InitializeComponent();
+    }
+
+    private void CalcularMedia(List<NotaSimulado> notas)
+    {
+        double media = notas.Average(u => u.Nota);
+        label_media.Text = media.ToString("N2");
+    }
+
+    private void CalcularMenorNota(List<NotaMateria> notas)
+    {
+        NotaMateria? menorNota = notas.MinBy(n => n.Nota);
+        if (menorNota != null)
+        {
+            label_menor_nota.Text = menorNota.Nota.ToString("N2");
+            label_menor_materia.Text = menorNota.Materia;
+        }
+    }
+
+    private void CalcularMaiorNota(List<NotaMateria> notas)
+    {
+        NotaMateria? maiorNota = notas.MaxBy(n => n.Nota);
+        if (maiorNota != null)
+        {
+            label_maior_nota.Text = maiorNota.Nota.ToString("N2");
+            label_maior_materia.Text = maiorNota.Materia;
+        }
+    }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
 
-        line_chart.ItemsSource = new ViewModelExemplo().NotaSimulados;
-        polar_line_chart.ItemsSource = new ViewModelExemplo().NotaMaterias;
+        var notasMaterias = new ViewModelExemplo().NotaMaterias;
+        var notasSimulados = new ViewModelExemplo().NotaSimulados;
 
+        line_chart.ItemsSource = notasSimulados;
+        polar_line_chart.ItemsSource = notasMaterias;
+        column_chart.ItemsSource = notasMaterias;
+
+        CalcularMedia(notasSimulados);
+        CalcularMenorNota(notasMaterias);
+        CalcularMaiorNota(notasMaterias);
     }
 
     private void MinhasMaterias_Tapped(object sender, TappedEventArgs e)
     {
-        DisplayAlertAsync("Ops!","Essa funcionalidade ainda não está disponível.", "OK");
+        DisplayAlertAsync("Ops!", "Essa funcionalidade ainda não está disponível.", "OK");
     }
 
+    private void ColumnChart_LabelCreated(object sender, ChartAxisLabelEventArgs e)
+    {
+        string labelText = e.Label;
+
+        if (labelText.Length > 3)
+        {
+            e.Label = labelText.Substring(0, 3);
+        }
+    }
 }
