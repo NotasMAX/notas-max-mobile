b568b9c3a2792caaa1e022ce9b4c59ac6bcad319
Evelyn Cassinotte <cassinotte@gmail.com>
2026-05-18 18:32:10 -0300

    feat: adicionada a view de meuSimulados com contato pela api

--- NotasMax/ContentsViews/MeuSimuladoItemContentView.xaml.cs
+++ NotasMax/ContentsViews/MeuSimuladoItemContentView.xaml.cs
@@ -1,16 +1,11 @@
-using NotasMax.Services.Simulados;
-using NotasMax.Services.NavigationService;
-using NotasMax.Services.Settings;
-using NotasMax.Services.Simulados;
+using NotasMax.ViewModels;
+using System.Globalization;
 
 namespace NotasMax.ContentsViews;
 
 public partial class MeuSimuladoItemContentView : ContentView
 {
-    private readonly ISimuladoService _simuladoService;
-    private readonly ISettingsService _settingsService;
-    private readonly INavigationService _navigationService;
-
+    bool isRealizado = false;
     public MeuSimuladoItemContentView()
 	{
 		InitializeComponent();
@@ -18,6 +13,41 @@ public partial class MeuSimuladoItemContentView : ContentView
 
     private void Button_Tapped(object sender, TappedEventArgs e)
     {
+        if (BindingContext is not SimuladosByAluno.Simulado simulado)
+            return;
+
+        if (!isRealizado)
+            return;
+
+        //lógica para ir para a view de simulado
+    }
+
+    protected override void OnBindingContextChanged()
+    {
+        base.OnBindingContextChanged();
+        if (BindingContext is not SimuladosByAluno.Simulado simulado)
+            return;
+
+        isRealizado = simulado.DataRealizacao <= DateTime.Now;
+
+        if (isRealizado)
+        {
+            border_icon.BackgroundColor = Color.FromArgb("#eaf8f0"); 
+            image_icon.Source = "done.png";
+            label_valor_media.TextColor = Helpers.ColorHelper.DefinirCor(simulado.Media);
+            label_valor_media.Text = simulado.Media.ToString("N2");
+        }
+        else
+        {
+            border_icon.BackgroundColor = Color.FromArgb("#eff0f5");
+            image_icon.Source = "clock.png";
+            container_media.IsVisible = false;
+            image_arrow.IsVisible = false;
+        }
+
+        label_numero_simulado.Text = $"Simulado {simulado.Numero}";
+        string dataFormatada = simulado.DataRealizacao.ToString("dd 'de' MMMM 'de' yyyy", new CultureInfo("pt-BR"));
+        label_data_realizacao.Text = dataFormatada;
 
     }
 }
